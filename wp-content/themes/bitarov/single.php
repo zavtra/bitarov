828dd2d01be49d732d31949106fa7767c17581c6
pas9x <spam@pascalhp.net>
2012-08-19 17:10:26 +0900

    убрал p из контактов

--- wp-content/themes/bitarov/single.php
+++ wp-content/themes/bitarov/single.php
@@ -9,12 +9,10 @@ get_header();
                 <div class='b-top-left'>
                     <div class='breadcrumbs'>
 <?php
-$categories_path = get_cat_path(bt_post_category($post->ID));
+$post_category_id = bt_post_category($post->ID);
 $breadcrumbs = "<span class='current'><a href='" . SITE_URL . "'><ins></ins>bitarov.as</a></span>";
-foreach ($categories_path as $cat)
- {
- $breadcrumbs .= "<span><a href='" . get_category_link($cat->term_id) . "'>{$cat->name}</a><ins class='r'></ins></span>";
- }
+foreach (get_cat_path($post_category_id) as $cat)
+  $breadcrumbs .= "<span><a href='" . get_category_link($cat->term_id) . "'>{$cat->name}</a><ins class='r'></ins></span>";
 echo "                    $breadcrumbs";
 ?>
                     </div>
@@ -26,15 +24,26 @@ echo "                    $breadcrumbs";
         <div class='event-bottom-img'></div>
         <div class='wrap'>
             <div class='wrp_article'>
+<?php
+$post_category = get_category($post_category_id);
+$post_category_link = get_category_link($post_category_id);
+$liked_arr = new WP_Query(array('posts_per_page'=>get_option('bt_liked_pp'), 'cat'=>$post_category_id, 'post__not_in'=>array($post->ID)));
+$liked = '';
+while ($liked_arr->have_posts())
+ {
+ $liked_arr->the_post();
+ $liked .= "<li><a href='" . get_permalink($post->ID) . "'>" . get_the_title() . "</a></li>\n                        ";
+ }
+if ($liked) echo <<<HTML
                 <div class='like_records'>
                     <h3><strong>похожие записи</strong></h3>
-                    <span>из рубрики <a href="#">Политическая ситуация</a></span>
+                    <span>из рубрики <a href="$post_category_link">{$post_category->name}</a></span>
                     <ul>
-                        <li><a href="#">Нужно активнее сотрудничать с крупными предприятиями и финансово- промышленными группами</a></li>
-                        <li><a href="#">Нового министра строительства будем оценивать по реальным делам</a></li>
-                        <li><a href="#">Другую должность даже не рассматривал</a></li>
+                        $liked
                     </ul>
                 </div>
+HTML;
+?>
                 <div class='article'>
                     <div class='date'><?php echo rusdate('j F Y', strtotime($post->post_date)); ?></div>
                     <?php the_post(); the_content(); ?>
