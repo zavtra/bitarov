9ab196ae73417635eb9fe4030fc02f897b3a6c89
Azizov Eldar <manchant@mail.ru>
2012-09-25 22:39:02 +0900

    счетчики

--- wp-content/themes/bitarov/footer.php
+++ wp-content/themes/bitarov/footer.php
@@ -18,6 +18,37 @@
         </div>
     </div>
 </div>
+<!-- Yandex.Metrika -->
+<div style="display:none;"><script type="text/javascript">
+(function(w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter2026081 = new Ya.Metrika(2026081);
+             yaCounter2026081.clickmap(true);
+             yaCounter2026081.trackLinks(true);
+        
+        } catch(e) {}
+    });
+})(window, 'yandex_metrika_callbacks');
+</script></div>
+<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript" defer="defer"></script>
+<noscript><div style="position:absolute"><img src="//mc.yandex.ru/watch/2026081" alt="" /></div></noscript>
+<!-- /Yandex.Metrika -->
 
+__________________________________________________________-
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20233722-1']);
+  _gaq.push(['_setDomainName', 'none']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 </body>
 </html>
